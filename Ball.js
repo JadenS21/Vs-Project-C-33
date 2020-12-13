bdc63e1412149ec389a977cc1d21eeec0c03921a
Jaden <jaden.serrao@gmail.com>
2020-12-13 17:03:57 +0530

    Plinko

--- Ball.js
+++ Ball.js
@@ -17,17 +17,5 @@ class Ball {
       ellipse(0, 0, this.radius);
       pop();
       
-      for(var i = 0;i <= width-60;i = i + 80){
-        ball1.push(new Ball(i,200))
-      }
-      for(var j = 0;j <= width;j = j + 80){
-        ball2.push(new Ball(j,200))
-      }
-      for(var k = 0;k <= width-60;k = k + 80){
-        ball3.push(new Ball(k,200))
-      }
-      for(var l = 0;l <= width;l = l + 80){
-        ball4.push(new Ball(l,200))
-      }
     }
   };
--- sketch.js
+++ sketch.js
@@ -28,6 +28,19 @@ function setup() {
   ball2 = new Ball(90,150,20);
   ball3 = new Ball(60,200,20);
   ball4 = new Ball(90,250,20);
+
+  for(var i = 0;i <= width-60;i = i + 80){
+    ball1.push(new Ball(i,200))
+  }
+  for(var j = 0;j <= width;j = j + 80){
+    ball2.push(new Ball(j,200))
+  }
+  for(var k = 0;k <= width-60;k = k + 80){
+    ball3.push(new Ball(k,200))
+  }
+  for(var l = 0;l <= width;l = l + 80){
+    ball4.push(new Ball(l,200))
+  }
   
 }
 
